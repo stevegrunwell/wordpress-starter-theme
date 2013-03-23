4c7fd60ea594cb3d23690376f724a7aa172f331a
Steve Grunwell <stevegrunwell@gmail.com>
2013-03-23 13:47:58 -0400

    Added a custom searchform.php template

---
+++ searchform.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Search form
+ * @package %Theme_Name%
+ * @author %Author%
+ */
+
+// Since we can have multiple search forms per page we should generate a unique element ID
+$search_input_id = sprintf( 'search-input-%s', uniqid() );
+
+?>
+
+<form method="get" action="<?php echo home_url( '/' ); ?>" class="search" role="search">
+  <label for="<?php echo $search_input_id; ?>" class="screen-reader-text"><?php _e( 'Search for:', '%Text_Domain%' ); ?></label>
+  <input type="text" name="s" id="<?php echo $search_input_id; ?>" value="<?php the_search_query(); ?>" />
+  <button type="submit" value="<?php echo esc_attr( __( 'Search', '%Text_Domain%' ) ); ?>"><?php _e( 'Search', '%Text_Domain%' ); ?></button>
+</form><!-- form.search -->
