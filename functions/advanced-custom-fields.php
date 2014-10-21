7ea20e36632adbd938b624d395857b029d4962b0
Steve Grunwell <stevegrunwell@gmail.com>
2014-10-21 09:11:12 -0400

    Setup default loading for ACF PHP exports

--- functions/advanced-custom-fields.php
+++ functions/advanced-custom-fields.php
@@ -7,6 +7,16 @@
  */
 
 /**
+ * Load the PHP export of your ACF config in non-development environments
+ *
+ * To prevent loading this file (e.g. in development), add the following to your wp-config.php:
+ * define('USE_LOCAL_ACF_CONFIGURATION', true);
+ */
+if ( ! defined( 'USE_LOCAL_ACF_CONFIGURATION' ) || ! USE_LOCAL_ACF_CONFIGURATION ) {
+  require_once dirname( __FILE__ ) . '/advanced-custom-field-export.php';
+}
+
+/**
  * Shortcut for `echo themename_get_custom_field( ... )`, accepts the same arguments
  * @param str $key The custom field key
  * @param int $id The post ID
